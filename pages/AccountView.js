12fc34bc9a6d23a358e7c9e3cc6f535189192825
repenaattori <reimarii@oamk.fi>
2026-02-25 21:03:47 +0200

    Account view changes

--- pages/AccountView.js
+++ pages/AccountView.js
@@ -7,36 +7,29 @@ export default function AccountView(){
     const {userdata} = useContext(AuthContext);
     const [nickname, setNickname] = useState(userdata ? userdata.nickname : '');
     const [decs, setDesc] = useState(userdata ? userdata.description : '');
+    const [email, setEmail] = useState(userdata ? userdata.email : '');
     
     if(!userdata){
         return  <Text variant="titleMedium">User data not found!!!</Text>
     }
 
     async function saveData(){
-        let error = await modifyUserData({nickname, description: decs});
-        Alert.alert('Error', error.message);
+        let error = await modifyUserData({nickname, description:decs, email});
+        if(error){
+            Alert.alert('Error', error.message);
+        }
     }
 
     return(
         <View style={styles.container}>
-            <Text variant="titleMedium">{userdata.email}</Text>
-            <TextInput
-                value={nickname}
-                label={'Nickname'}
-                onChangeText={setNickname}
-            />
-            <TextInput
-                value={decs}
-                label={'Description'}
-                onChangeText={setDesc}
-            />
-            <Button 
-                mode={"contained"} 
-                onPress={saveData}>
+            <TextInput value={email} label={'Email'} onChangeText={setEmail}/>
+            <TextInput value={nickname} label={'Nickname'} onChangeText={setNickname}/>
+            <TextInput value={decs} label={'Description'} onChangeText={setDesc}/>
+            <Button mode={"contained"} onPress={saveData}>
                     Save user data
             </Button>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -47,3 +40,4 @@ const styles = StyleSheet.create({
     gap: 10
   },
 });
+
